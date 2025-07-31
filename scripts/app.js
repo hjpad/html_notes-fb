22b7110aedf2a3334994b0ce45e3bab50c29e708
hjpad <116934207+hjpad@users.noreply.github.com>
2025-07-31 09:02:00 +0000

    Deploying to gh-pages from @ hjpad/html_notes-fb@2c52d5e4beb3d943e78fe6e66ba9712dff47f592 🚀

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,6 @@
 
 // Firebase initialization
-const firebaseConfig = {
-  apiKey: ""AIzaSyA5C_arWOof0iA4LWOrnNTICurTWG__cX0"",
-  authDomain: ""html-notes-fb.firebaseapp.com"",
-  projectId: ""html-notes-fb"",
-  storageBucket: ""html-notes-fb.firebasestorage.app"",
-  messagingSenderId: ""807374533740"",
-  appId: ""1:807374533740:web:f840bfcb623149dc7fd449""
-};
+const firebaseConfig = window.firebaseConfig;
 
 firebase.initializeApp(firebaseConfig);
 // Initialize App Check
